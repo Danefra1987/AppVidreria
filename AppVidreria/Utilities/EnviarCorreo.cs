ef9dc78df53393b11f72302cfe069d5c2b9e4a45
Daniel Granizo <daneframetal@hotmail.com>
2023-08-28 21:56:04 -0500

    Metodo para envio de correo

--- AppVidreria/Utilities/EnviarCorreo.cs
+++ AppVidreria/Utilities/EnviarCorreo.cs
@@ -8,5 +8,34 @@ namespace AppVidreria.Utilities
 {
     internal class EnviarCorreo
     {
+        public EnviarCorreo() { 
+        
+        }
+
+        public async void EnviarCorreoCotizacion(string strSubjet, string strBody, string[] strCorreos)
+        {
+            if (Email.Default.IsComposeSupported)
+            {
+
+                string subject = "Hello friends!";
+                string body = "Esto es una prueba de envio de correo";
+                string[] recipients = new[] { "daneframetal@hotmail.com", "esneider_98@outlook.com" };
+
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    BodyFormat = EmailBodyFormat.PlainText,
+                    To = new List<string>(recipients)
+                };
+
+                ///Poner aqui el nombre del archivo pdf 
+                string picturePath = Path.Combine(FileSystem.CacheDirectory, "memories.jpg");
+
+                message.Attachments.Add(new EmailAttachment(picturePath));
+
+                await Email.Default.ComposeAsync(message);
+            }
+        }
     }
 }
