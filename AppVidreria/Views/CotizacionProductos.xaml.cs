7ded10096f4865b2c31185f63403405b5def075e
Daniel Granizo <daneframetal@hotmail.com>
2023-09-06 00:11:51 -0500

    Compartir

--- AppVidreria/Views/CotizacionProductos.xaml.cs
+++ AppVidreria/Views/CotizacionProductos.xaml.cs
@@ -19,5 +19,46 @@ public partial class CotizacionProductos : ContentPage
         InitializeComponent();
         this.cotizacionProductosList = cotizacionProductosList;
         lvCotizacionProductos.ItemsSource = this.cotizacionProductosList;
+
+        decimal total = 0m;
+
+        foreach (var item in cotizacionProductosList)
+        {
+            total = total + item.Precio;
+        }
+        txtValorTotal.Text = total.ToString();
+    }
+
+    private async void BtnCompartir_Clicked(object sender, EventArgs e)
+    {
+        var file =await  CompartirPantalla(Capture);
+        await ShareFile(file);
+    }
+
+    public async Task<string> CompartirPantalla(View contenedor)
+    {
+        if (Screenshot.Default.IsCaptureSupported)
+        {
+            var screen = await contenedor.CaptureAsync();
+            Stream stream = await screen.OpenReadAsync(ScreenshotFormat.Png, 100);
+            string fileDestino = Path.Combine(FileSystem.Current.AppDataDirectory, "capture.png");
+            using (var fileStream = File.Create(fileDestino))
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.CopyTo(fileStream);
+            }
+            return fileDestino;
+        }
+        return "";
+
+    }
+
+    public async Task ShareFile(string file)
+    {
+        await Share.Default.RequestAsync(new ShareFileRequest
+        {
+            Title = "Cotizacion",
+            File = new ShareFile(file)
+        });
     }
 }
